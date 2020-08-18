a4db331518a460cbc0646a35b539d2e474b92282
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-08-18 15:52:52 -0500

    Replace useState with zustand for state management

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { v4 as uuid } from 'uuid';
+import create from 'zustand';
 import Badge from './components/Badge.js';
 import TodoList from './components/TodoList.js';
 import TodoInput from './components/TodoInput.js';
 import './App.css';
 
+const useStore = create(set => ({
+  todos: [{ id: uuid(), text: 'Finish Todo App' }],
+  addTodo: (newTodo) => set((state) => ({ todos: [...state.todos, { id: uuid(), text: newTodo }] })),
+  removeTodo: (id) => set((state) => ({ todos: state.todos.filter((td) => td.id !== id) }))
+}));
+
 const App = () => {
-  const [todos, setTodos] = useState([{ id: uuid(), text: 'Finish Todo App' }]);
-  const addTodo = (newTodo) => setTodos([...todos, { id: uuid(), text: newTodo }]);
-  const removeTodo = (id) => setTodos(todos.filter((td) => td.id !== id));
+  const todos = useStore(state => state.todos);
+  const addTodo = useStore(state => state.addTodo);
+  const removeTodo = useStore(state => state.removeTodo);
   return (
     <div className="app">
       <h1 className="header">Todo List <Badge number={todos.length} /></h1>
